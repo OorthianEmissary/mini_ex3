60a8e5884a9ffd03b8fa8bafb69356e7490f2e83
Oorthian Emissary <Vixenousvermingod@hotmail.com>
2017-02-22 15:31:53 +0100

    Throbber v2
    Correct code this time. Or more correct at least.

--- Throbber/sketch.js
+++ Throbber/sketch.js
@@ -8,25 +8,26 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
     frameRate(20);
-    background(10)
+    background(10);
+    console.log("Hello World");
     
     
 }
 
 function draw() {
     noStroke();
+    throbber(5);
     fill(10, 10, 10, 255/7);
-    rect(0, 0, windowWidth, windowHeight)
-    console.log("Hello World")
+    rect(0, 0, windowWidth, windowHeight);
     
 }
 
-function throbber() {
+function throbber(num) {
     
     //rotating pepe
     push();
     translate(windowWidht/2, windowHeigh/2);
-    var cir = 360/9*(frameRate%9);
+    var cir = 360/num*(frameRate%num);
     rotate(radians(cir));
     image(pepe, 0, 0);
     pop();
